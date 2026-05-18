9b341d726a991183c4adbc527ccdf62e05ffdeb4
M.Haroon <muhammadharoon2004059@gmail.com>
2026-05-19 00:31:32 +0500

    Add files via upload

---
+++ Chapter 43 to 52/Chapter 49 to 52/app.js
@@ -0,0 +1,132 @@
+// Chapter 49 to 52 (Q1)
+
+// const myForm = document.getElementById("signupForm");
+// const displayArea = document.getElementById("displayArea");
+
+// myForm.addEventListener('submit', function(event) {
+//     event.preventDefault();
+
+//     const name = document.getElementById('username').value;
+//      const email = document.getElementById('useremail').value;
+
+//      displayArea.innerHTML = '<p><strong>Name:</strong> ${name}</p>'
+//                              '<p><strong>Email:</strong> ${email}</p>'
+// });
+
+// Chapter 49 to 52 (Q2)
+
+// function greet (name){
+//     console.log('hello' + name);
+// }
+
+// greet('ali');
+// greet('ahmed');
+
+
+// function add(a,b) {
+//     return a + b;
+// } 
+
+// let result = add(2,3);
+// console.log(result)
+
+
+// function test() {
+//     return 'hello';
+//     console.log()
+// }
+
+
+
+// function greet(name) {
+//     console.log("welcome " + name);
+// }
+
+// greet('ali')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sum(a,b) {
+//     return a + b;
+// }
+
+// var result = (5+5);
+// console.log(result);
+
+
+// var user = prompt('enter your age');
+// function check(name,age) {
+//     if (age < 18) {
+//         console.log('not allowed')
+//     } else {
+//         console.log('welcome' + name)
+//     }
+// }
+
+// check('ali', 17);
+// check('ahmed', 18);
+
+
+
+// function add(a,b) {
+//     console.log(a+b);
+// }
+
+// add(2,3);
+
+
+// function sum(a,b) {
+//     return a+b;
+// }
+
+// let result = sum(5,5);
+// console.log(result)
+
+
+
+// function sum(a,b) {
+//    console.log(a+b);
+// }
+
+// sum(5,5);
+
+
+
+// function add(a,b){
+//     console.log(a + b);
+// }
+
+// function multiply(a,b){
+//     console.log(a * b);
+// }
+
+// let result = add(2,3);
+// multiply(result, 5);
+
+
+
+// function add(a,b){
+//     return a + b;
+// }
+
+// function multiply(a,b){
+//    return a * b;
+// }
+
+// let result = add(2,3);
+// let final = multiply(result, 5);
+
+// console.log(final);
